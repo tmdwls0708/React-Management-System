ae905c63182a1aa14ab879881c0b18d346e6ccbc
sjkim@jness.co.kr <sjkim@jness.co.kr>
2020-02-04 17:53:39 +0900

    customer map 반복

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Let's develop management system!!</h2>
-      </header>
-    </div>
-  );
+
+const customers =  [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '나동빈',
+    'birthday': '960508',
+    'gender': '남자',
+    'job': '프로그래머'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이순신',
+    'birthday': '961127',
+    'gender': '남자',
+    'job': '디자이너'
+  }
+]
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        {customers.map(x=>{
+          return <Customer key={x.id} id={x.id} image={x.image} name={x.name} birthday={x.birthday} gender={x.gender} job={x.job} />
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
